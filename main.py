beabf17ad9419ccbb4824a6fd88c054a30ba8853
Rish-14S <rishank.sharma.1426@gmail.com>
2024-12-29 19:29:00 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, accuracy_score
+from sklearn.preprocessing import StandardScaler
+
+data = pd.read_csv("creditcard.csv")
+
+X = data.drop('Class', axis=1)
+y = data['Class']
+
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+
+model = RandomForestClassifier(random_state=42, n_estimators=100, max_depth=10)
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print(classification_report(y_test, y_pred))
